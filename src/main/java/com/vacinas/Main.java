f0e006658a94186c12bf51ab613dacacb8837e6d
Adevalter <adevalter@hotmail.com>
2025-02-13 16:29:59 -0300

    Branch desenvolvimento

--- src/main/java/com/vacinas/Main.java
+++ src/main/java/com/vacinas/Main.java
@@ -3,16 +3,10 @@ package com.vacinas;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-import com.vacinas.DAO.ConexaoDAO;
-
 public class Main {
     public static Connection conexao = null;
 
     public static void main(String[] args) throws SQLException {
-
-        conexao = ConexaoDAO.getConexao();
-
-        System.out.println(conexao);
 
         System.out.println("Hello world!");
     }
